bae487e911a513e99d29a1d8beff838650ce141e
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-05-31 20:14:34 -0300

    Update conexion.php

--- TrabajoFinal/utils/conexion.php
+++ TrabajoFinal/utils/conexion.php
@@ -1,2 +1,14 @@
 <?php
+    //CREADO LA CLASE DE CONEXION
+    class Conexion
+    {
+        //AGREGO LA CONEXION A LA BASE DE DATOS
+        var $conn;
+
+        function __construct()
+        {
+            $this -> conn= new mysqli("db4free.net","admcisterna","cisterna2019nqn","cisterna");
+        }
+
+    }
 ?>
