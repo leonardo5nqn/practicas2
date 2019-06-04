95e7a58bea5431fad92b0b3fb0ce00bc55860695
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-06-04 19:22:06 -0300

    Update controllerUsuario.php

--- TrabajoFinal/controller/controllerUsuario.php
+++ TrabajoFinal/controller/controllerUsuario.php
@@ -1,18 +1,22 @@
 <?php
 include "Usuario.php";
 
-$_POST ['Usuario'];
-$_POST ['Password'];
-
-if (Usuario :: validoUsuario ($_POST["Usuario"],$_POST["Password"]))
+if(!empty($_POST['Usuario']) && !empty($_POST['Password'])) 
 {
+    if (Usuario :: validoUsuario ($_POST["Usuario"],$_POST["Password"]))
+    {
     print_r($_POST["Usuario"],$_POST["Password"]);
+    }
+    else 
+    {
+    echo "contrasena o usuario incorrecto";
+    }
+    else 
+    {
+        echo "<p> Es necesario que completes tu constrasena.</p>";
+    }
 }
 
-else 
-{
-    echo "contrasena o usuario incorrecto";
-}
 exit();
 
 ?>
