92dff80eeb2eaadf64f390cc71e4e79afb190c16
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-06-13 15:14:02 -0300

    Update controllerVehiculo.php

--- TrabajoFinal/controller/controllerVehiculo.php
+++ TrabajoFinal/controller/controllerVehiculo.php
@@ -1,42 +1,57 @@
 <?php
 include "../model/Vehiculo.php";
-
-class VehiculoController{
-
-static function insertVehiculo(){
-    if(!empty($_POST['Patente']) && !empty($_POST['Marca']) && !empty($_POST['Modelo'])&& !empty($_POST['Color'])&& !empty($_POST['TipoVehiculo'])) 
-{
-    if (Vehiculo::insertVehiculo($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']))
+if($_POST["accion"]==='nuevo'){
+    if (!empty($_POST['Patente']) && !empty($_POST['Marca']) && !empty($_POST['Modelo'])&& !empty($_POST['Color'])&& !empty($_POST['TipoVehiculo']))
     {
-    print_r($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']);
+        if (Vehiculo::insertVehiculo($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']))
+        {
+        print_r($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']);
+        }
+    }
+    else
+    {
+        echo "es necesario que completes los campos";
     }
 }
-else 
-{
-    echo "es necesario que completes los campos";
-}
-}
 
-/* 
-static function eliminarVehiculo(){
-    Conexion::conectar()->query("DELETE from Vehiculo where IDVehiculo = $id");
-}
 
-static function editarVehiculo(){
-    Conexion::conectar()->query("UPDATE Vehiculo set Patente = '$numPatente', Marca = '$marca', Modelo = '$modelo', Color = '$color', TipoVehiculo = '$tipoVehiculo'
-        where IDVehiculo = $vehiculoID");
-}
-
-static function getAllVehiculos()
-{
-if ($VehiculoID = "IDVehiculo")
-{
-    if (Vehiculo::deleteVehiculo ($_POST["IDVehiculo"]))
+if($_POST["accion"]==='eliminar'){
+    if ($VehiculoID = "IDVehiculo")
     {
-        print_r($_POST["IDVehiculo"]);
+        if (Vehiculo::deleteVehiculo ($_POST["IDVehiculo"]))
+    {
+        echo 'vehiculo eliminado';
     }
+    }
+    else
+    {
+        echo "error, no se pudo eliminar el vehiculo";
+    }
+}
 
+
+
+if($_POST["accion"]==='editar'){
+    if ($VehiculoID = "IDVehiculo")
+    {
+        if (Vehiculo::deleteVehiculo ($_POST["IDVehiculo"]))
+    {
+        print_r($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']); 
+    }
+    }
 }
-} */
+
+
+if($_POST["accion"]==='mostrar'){
+    if ($VehiculoID = "IDVehiculo")
+    {
+        if (Vehiculo::deleteVehiculo ($_POST["IDVehiculo"]))
+    {
+       Vehiculo::getAllVehiculos();
+    }
+    }
 }
+
+
+
 ?>
