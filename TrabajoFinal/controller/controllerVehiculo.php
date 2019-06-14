b9bb53b8c36c10adbfb2d327eeafc54975f83272
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-06-13 21:40:08 -0300

    Update controllerVehiculo.php

--- TrabajoFinal/controller/controllerVehiculo.php
+++ TrabajoFinal/controller/controllerVehiculo.php
@@ -1,10 +1,10 @@
 <?php
+//llamo a la calse model vehiculo
 include "../model/Vehiculo.php";
 
-$a="accion";
-    switch ($a){
-        case ($a = 'nuevo'):
-        if($_POST["accion"]==='nuevo'){
+    switch ($_POST["accion"]){
+        case ('nuevo'):
+       
             if (!empty($_POST['Patente']) && !empty($_POST['Marca']) && !empty($_POST['Modelo'])&& !empty($_POST['Color'])&& !empty($_POST['TipoVehiculo']))
             {
                 if (Vehiculo::insertVehiculo($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']))
@@ -16,11 +16,11 @@ $a="accion";
             {
                 echo "es necesario que completes los campos";
             }
-        }
+        
         break;
-        case ($a = 'eliminar'):
-        if($_POST["accion"]==='eliminar'){
-            if ($VehiculoID = "IDVehiculo")
+        case ('eliminar'):
+        
+            if ($_POST["Vehiculoid"] = "IDVehiculo")
             {
                 if (Vehiculo::deleteVehiculo ($_POST["IDVehiculo"]))
             {
@@ -31,29 +31,29 @@ $a="accion";
             {
                 echo "error, no se pudo eliminar el vehiculo";
             }
-        }
+        
         break;
-        case ($a = 'editar'):
-        if($_POST["accion"]==='editar'){
-            if ($VehiculoID = "IDVehiculo")
+        case ('editar'):
+        
+            if ( $_POST["Vehiculoid"] = "IDVehiculo")
             {
                 if (Vehiculo::updateVehiulo ($_POST["IDVehiculo"]))
             {
                 print_r($_POST["Patente"],$_POST["Marca"],$_POST['Modelo'],$_POST['Color'],$_POST['TipoVehiculo']); 
             }
             }
-        }
+        
         break;
-        case ($a = 'mostrar'):
-        if($_POST["accion"]==='mostrar'){
-            if ($VehiculoID = "IDVehiculo")
+        case ('mostrar'):
+        
+            if ($_POST["Vehiculoid"]  = "IDVehiculo")
             {
                 if (Vehiculo::getAllVehiculos())
             {
                Vehiculo::getAllVehiculos();
             }
             }
-        }
+        
         break;   
     }
 ?>
