6c7e6afded682b1d0b5630ed8cd7a289c0c73bce
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-06-25 11:52:48 -0300

    Update Usuario.php

--- TrabajoFinal/model/Usuario.php
+++ TrabajoFinal/model/Usuario.php
@@ -2,7 +2,7 @@
     //LLAMO A LA CLASE CONEXION
     require_once ("../utils/conexion.php");
 
-    class Usuario
+    class Usuario extends Persona
     {
         private  $usuario;
         private  $password;
@@ -13,16 +13,22 @@
        
 
         //constructor
-        function __construct()
-        {
-        }
+        public function __construct($_usuario, $_password, $_idUsuario,$_idPersona,$_idRol,$_huella)
+     {
+      $this->usuario = $_usuario;
+      $this->password = $_password;
+      $this->idUsuario = $_idUsuario;
+      $this->idPersona = $_idPersona;
+      $this->idRol = $_idRol;
+      $this->huella = $_huella;
+      
+     }
 
         //metodos get y set Usuario
         public function getUsuario()
         {
             return $this->usuario;
         }
-     
         public function setUsuario($us)
         {
             $this->usuario = $us;
@@ -33,30 +39,65 @@
             return $this->password;
         }
      
-        public function setUsuario($pass)
+        public function setPassword($pass)
         {
             $this->password = $pass;
         }
+        //metodos get y set idUsuario
+        public function getIDUsuario()
+        {
+            return $this->idUsuario;
+        }
+        
+        //metodos get y set idPersona
+        public function getidPersona()
+        {
+            return $this->idPersona;
+        }
+     
+        public function setidPersona($idpersona)
+        {
+            $this->idPersona = $idpersona;
+        }
+        //metodos get y set idRol
+        public function getidRol()
+        {
+            return $this->idRol;
+        }
+     
+        public function setidRol($idrol)
+        {
+            $this->idRol = $idrol;
+        }
         
         //Crear nuevo usuario
-        static function insert()
+        function insert()
         {
             $respuesta = Conexion::conectar()->query("INSERT INTO Usuario (PersonaID, RolID, NombreUsuario, Contrasena, Huella)
             values (".$this->idUsuario.",".$this->idRol.",".$this->usuario.",".$this->password." ,".$this->huella." )");
             return ($respuesta);
         }
         //eliminar usuario
-        static function delete()
+        function delete()
         {
             $respuesta = Conexion::conectar()->query("DELETE FROM Usuario where IDUsuario =".$this->idUsuario."");
             return ($respuesta);
         }
         //editar usuario
-        static function update()
+        function update()
         {
             $respuesta = Conexion::conectar()->query("UPDATE Usuario set  NombreUsuario =".$this->usuario.", Contrasena =".$this->password." 
             where IDUsuario =".$this->idUsuario."");
             return ($respuesta);
+        }
+
+        static function findByID($id){
+            return (Conexion::conectar()->query("SELECT * FROM Usuario WHERE IDUsuario = ".$id));    
+        } 
+
+        static function listarUsuario($where)
+        {
+            return (Conexion::conectar()->query("SELECT * FROM Usuario ".$where));
         }
 
            /* static function validoUsuario($usuario,$password){
