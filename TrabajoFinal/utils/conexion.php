906f153ee7e641fe3b6f357bec89b2ed7959d248
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-09-09 22:43:43 -0300

    Update conexion.php

--- TrabajoFinal/utils/conexion.php
+++ TrabajoFinal/utils/conexion.php
@@ -3,34 +3,30 @@
     class Conexion
     {
         private static $instance = null;
-        exit('asd');
         
         //AGREGO LA CONEXION A LA BASE DE DATOS
-        private $host ="db4free.net";
-        private $usuario="admcisterna";
-        private $pass="cisterna2019nqn";
-        private $nameBD="cisterna";
+        private static $host ="db4free.net:3306";
+        private static $usuario="admcisterna";
+        private static $pass="cisterna2019nqn";
+        private static $nameBD="cisterna";
 
         function __construct()
         {
-            self::$instance = new mysqli($this->host,$this->usuario,$this->pass,$this->nameBD);
+            self::$instance = new mysqli(self::$host,self::$usuario,self::$pass,self::$nameBD);
         }
 
        public static function conectar() {
-        var_dump(self::$instance);
-        exit();
-             if(self::$instance === null){
-                 exit('asd');
-                 self::$instance = new Conexion();
-            }
-            return self::$instance;
+             if(self::$instance == null){
+                new Conexion();
+             }
+             return self::$instance;
         }
         
         public static function cerrarConexion(){
             self::$instance->session_write_close();
             self::$instance = null;
         }
-        
-        
+    
+    
     }
 ?>  
