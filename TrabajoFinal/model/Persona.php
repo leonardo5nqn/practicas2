8906cc7c13af894239b0b11f7198fd439bb5d04a
Damianmartinez98 <50251384+Damianmartinez98@users.noreply.github.com>
2019-09-12 23:13:00 -0300

    Update Persona.php

--- TrabajoFinal/model/Persona.php
+++ TrabajoFinal/model/Persona.php
@@ -15,7 +15,17 @@ class Persona
     private $Email;
 
     //constructor
-    public function __construct($_personaid, $_Nombre, $_apellido,$_Telefono,$_documento,$_FechaNacimiento,$_Domicilio,$_Email )
+    public function __construct($_Nombre, $_apellido, $_Telefono, $_documento, $_FechaNacimiento, $_Domicilio, $_Email )
+   {
+      $this->setNombre($_Nombre);
+      $this->setApellido($_apellido);
+      $this->setTelefono($_Telefono);
+      $this->setDoc($_documento);
+      $this->setFNacimiento($_FechaNacimiento);
+      $this->setDomicilio($_Domicilio);
+      $this->setEmail($_Email);
+   }
+    private function __construct($_personaid, $_Nombre, $_apellido,$_Telefono,$_documento,$_FechaNacimiento,$_Domicilio,$_Email )
    {
       $this->setpersonaid($_personaid);
       $this->setNombre($_Nombre);
@@ -114,13 +124,28 @@ class Persona
       {
          $respuesta = Conexion::conectar()->query("INSERT INTO Persona (IDPersona, Nombre, Apellido, Telefono, Documento, FechaNacimiento, Domicilio, Email)
          values (".$this->getpersonaid().",".$this->getNombre().",".$this->getApellido().",".$this->getTelefono().",".$this->getDoc().",".$this->getFNacimiento().",".$this->getDomicilio().",".$this->getEmail().")");
-         return ($respuesta);    
+         
+        $rs= mysql_query( $respuesta); 
+
+        if ($rs == false ){
+            echo "error";
+        }     
+        else {
+            echo "se inserto";
+        }  
       }  
      //eliminar persona
      function delete()
       {
          $respuesta = Conexion::conectar()->query("DELETE FROM Persona where IDPersona =".$this->getpersonaid()."");
-         return ($respuesta);
+        $rs= mysql_query( $respuesta); 
+
+        if ($rs == false ){
+            echo "error";
+        }     
+        else {
+            echo "se elimino";
+        }
       }
 
      //editar persona
@@ -129,28 +154,46 @@ class Persona
          $respuesta = Conexion::conectar()->query("UPDATE Persona set  Nombre =".$this->getNombre().", Apellido =".$this->getApellido().", Telefono =".$this->getTelefono().", Documento =".$this->getDoc().",
          FechaNacimiento =".$this->getFNacimiento().", Domicilio =".$this->getDomicilio().", Email =".$this->getEmail()."
          where IDPersona =".$this->getpersonaid()."");
-         return ($respuesta);
+         
+        $rs= mysql_query( $respuesta); 
+
+        if ($rs == false ){
+            echo "error";
+        }     
+        else {
+            echo "se modifico";
+        }
       }
       static function findByID($id)
       {
          $respuesta = Conexion::conectar()->query("SELECT * FROM Persona WHERE IDPersona = ".$id);  
-         while ($fila = $respuesta -> fetch_object()){
-            $result[]= $fila; 
-        }
-        return $result[];  
+         if ($respuesta->rowCount() > 0) { 
+            $persona = array();
+            while ($row = $respuesta->fetch()) { 
+               array_push($persona, new Persona($row['IDPersona'],$row['Nombre'], $row['Apellido'], $row['Telefono'], $row['Documento'], $row['FechaNacimiento'], $row['Domicilio'], $row['Email']));
+            }
+         return ($persona);
+         }    
+         else { 
+            return ("No hay registros."); 
+         }
+          }
       } 
 
       static function listarPersona($where)
       {
          $respuesta = Conexion::conectar()->query("SELECT * FROM Persona ".$where);
-         while ($fila = $respuesta -> fetch_object()){
-            $result[]= $fila; 
-        }
-        return $result[];
+         if ($respuesta->rowCount() > 0) { 
+            $persona = array();
+            while ($row = $respuesta->fetch()) { 
+               array_push($persona, new Persona($row['IDPersona'],$row['Nombre'], $row['Apellido'], $row['Telefono'], $row['Documento'], $row['FechaNacimiento'], $row['Domicilio'], $row['Email']));
+            }
+         return ($persona);
+         }    
+         else { 
+            return ("No hay registros."); 
+         }
+          }
       } 
-       
-      
-
-
 }
 ?>
