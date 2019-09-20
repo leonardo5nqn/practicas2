84aa9405a9c53d88b09d4700d8862d34209ad3a7
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-09-20 17:49:55 -0300

    Update Empresa.php

--- TrabajoFinal/model/Empresa.php
+++ TrabajoFinal/model/Empresa.php
@@ -11,76 +11,122 @@ private $Direccion;
 private $Telefono;
 
 //Constructor vacio
-function __construct($IDEmpresa, $razonSocial, $Cuit, $Direccion, $Telefono)
+function __construct($IDEmpresa, $_razonSocial, $_Cuit, $_Direccion, $_Telefono)
 {
     $this->setIDEmpresa($IDEmpresa);
-    $this->setRazonsocial($razonSocial);
-    $this->setCuit($Cuit);
-    $this->setDireccion($Direccion);
-    $this->setTelefono($Telefono);
+    $this->setRazonsocial($_razonSocial);
+    $this->setCuit($_Cuit);
+    $this->setDireccion($_Direccion);
+    $this->setTelefono($_Telefono);
 
 }
 //Get/Set atributos
 public function getIDEmpresa(){
     return $this->IDEmpresa;
 }
-private function setIDEmpresa($Id){
-    $this->IDEmpresa = $Id;
+private function setIDEmpresa($_IdEmpresa){
+    $this->IDEmpresa = $_IdEmpresa;
 }
 
 public function getRazonSocial(){
     return $this->razonSocial;
 }
-private function setRazonsocial($rSocial){
-    $this->razonSocial = $rSocial;
+private function setRazonsocial($_rSocial){
+    $this->razonSocial = $_rSocial;
 }
 public function getCuit(){
     return $this->Cuit;
 }
-private function setCuit($setcuit){
-    $this->Cuit=$setcuit;
+private function setCuit($_setcuit){
+    $this->Cuit=$_setcuit;
 }
 public function getDireccion(){
     return $this->Direccion;
 }
-private function setDireccion($dire){
-    $this->Direccion=$dire;
+private function setDireccion($_dire){
+    $this->Direccion=$_dire;
 }
 
 public function getTelefono(){
     return $this->Telefono;
 }
-private function setTelefono($tel){
-    $this->Telefono=$tel;
+private function setTelefono($_tel){
+    $this->Telefono=$_tel;
 } 
 
 //Alta
 
 function insert(){
-    $rta = Conexion::conectar()->query("INSERT INTO Empresa (IDEmpresa, razonSocia, Cuit, Direccion, Telefono ) 
-    values (".$this->IDEmpresa.",".$this->razonSocial.",".$this->Cuit.",".$this->Direccion.",".$this->Telefono."");
-    return ($rta);
+    $resultado = Conexion::conectar()->query("INSERT INTO Empresa (IDEmpresa, razonSocial, Cuit, Direccion, Telefono ) 
+    values ('".$this->IDEmpresa."','".$this->razonSocial."','".$this->Cuit."','".$this->Direccion."','".$this->Telefono."')");
+    
+    //verifico si Inserto por el ID
+    $resulID = mysqli_insert_id(Conexion::conectar());
+    $this->setIDEmpresa($resulID);
+    return true;
 }
 //Baja
 function delet(){
-    $rta = Conexion::conectar()->query("DELETE FROM Empresa where IDEmpresa =".$this->IDEmpresa."");
-    return ($rta);
+    $resultado = Conexion::conectar()->query("DELETE FROM Empresa where IDEmpresa =".$this->getIDEmpresa()."");
+    return true;
 }
 
 //Modificación
 function update(){
-    $rta = Conexion::conectar()->query("UPDATE Empresa set razonSocial =".$this->razonSocial.", Cuit =".$this->Cuit.", Direccion =".$this->Direccion.", Telefono =".$this->Telefono.",
-     where IDEmpresa =".$this->IDEmpresa."");
-    return ($rta);
+    $resultado = Conexion::conectar()->query("UPDATE Empresa set razonSocial ='".$this->getRazonSocial()."', Cuit ='".$this->getCuit()."', Direccion ='".$this->getDireccion()."', Telefono ='".$this->getTelefono()."'  where IDEmpresa =".$this->getIDEmpresa());
+
+    return true;
 
     
 }
+
+public static function findAll (){
+    $resultado = Conexion::conectar()->query("SELECT * FROM Empresa");
+
+    if($resultado->num_rows > 0){
+        $empresa=array();
+        while ($row=$resultado->fetch_assoc()) {
+            array_push($empresa,new Empresa($row['IDEmpresa'],$row['razonSocial'],$row['Cuit'],$row['Direccion'],$row['Telefono']));
+        }
+        return ($empresa);
+    }
+    else{
+        return ("No hay registros");
+    }
+    
+}
+
+
+
 static function findByID($id){
-    return (Conexion::conectar()->query("SELECT * FROM Empresa WHERE IDEmpresa = ".$id));    
+    $resultado=Conexion::conectar()->query("SELECT * from Empresa where IDEmpresa = ".$id);
+    if($resultado->num_rows > 0){
+        $empresa=array();
+        while ($row=$resultado->fetch_assoc()) {
+            array_push($empresa,new Empresa($row['IDEmpresa'],$row['razonSocial'],$row['Cuit'],$row['Direccion'],$row['Telefono']));
+        }
+        return ($empresa);
+    }
+    else{
+        return ("No hay registros");
+    }
+
+    //return (Conexion::conectar()->query("SELECT * FROM Empresa WHERE IDEmpresa = ".$id));    
 }    
 
 static function listarEmpresa($where){
-  return (Conexion::conectar()->query("SELECT * FROM Empresa ".$where));
+    $resultado=Conexion::conectar()->query("SELECT * from Empresa where ".$where);
+    if($resultado->num_rows > 0){
+        $empresa=array();
+        while ($row=$resultado->fetch_assoc()) {
+            array_push($empresa,new Empresa($row['IDEmpresa'],$row['razonSocial'],$row['Cuit'],$row['Direccion'],$row['Telefono']));
+        }
+        return ($empresa);
+    }
+    else{
+        return ("No hay registros");
+    }
+    //return (Conexion::conectar()->query("SELECT * FROM Empresa ".$where));
 }
 
 }
