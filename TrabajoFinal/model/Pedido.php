9cf690dabe06a012e85e27506f3c4ff016ad700e
Nantiare <faguerlsc@gmail.com>
2019-09-19 22:23:15 -0300

    Update Pedido.php

--- TrabajoFinal/model/Pedido.php
+++ TrabajoFinal/model/Pedido.php
@@ -11,21 +11,14 @@ class Pedido{
     private $descripcion;
     private $fechaHora;
 
-    public function __construct($_solicitante, $_usuario, $_descripcion, $_fechaHora){
-      $this->setSolicitante($_solicitante->getID());
-      $this->setUsuario($_usuario->getID());
+    public function __construct($_pedidoID,$_solicitante, $_usuario, $_descripcion, $_fechaHora){
+      $this->setID($_pedidoID);
+      $this->setSolicitante($_solicitante);
+      $this->setUsuario($_usuario);
       $this->setDescripcion($_descripcion);
       $this->setFechaHora($_fechaHora);
     }
     
-    private function __construct($_pedidoID, $_solicitanteID, $_usuarioID, $_descripcion, $_fechaHora){
-      $this->setID($_pedidoID);
-      $this->setSolicitante($_solicitanteID);
-      $this->setUsuario($_usuarioID);
-      $this->setDescripcion($_descripcion);
-      $this->setFechaHora($_fechaHora);
-   }
-     
    private function setID($_id){
       $this->IDPedido = $_id;
      }
@@ -34,16 +27,16 @@ class Pedido{
       return $this->pedidoID;
    }
   
-     public function setSolicitante($_solicitanteID){
-        $this->solicitante = Solicitante::findByID($_solicitanteID);
+     public function setSolicitante($_solicitante){
+        $this->solicitante = $_solicitante;
      }
 
      public function getSolicitante(){
         return $this->solicitante;
      }
 
-     public function setUsuario($_usuarioID){
-        $this->usuario = Usuario::findByID($_usuarioID);
+     public function setUsuario($_usuario){
+        $this->usuario = $_usuario;
      }
 
      public function getusuario(){
@@ -66,60 +59,79 @@ class Pedido{
         return $this->fechaHora;
      }
 
-     public function insert(){
-      $query = Conexion::conectar()->query("INSERT INTO Pedido VALUE (".$this->getID().", ".$this->getSolicitante()->getID().", ".$this->getusuario()->getID().", ".$this->getDescripcion().", ".$this->getFechaHora().")");
-      if( $query === TRUE) {
+     public function insert (){
+      $conexion = Conexion::conectar();
+      $resultado = $conexion->query("INSERT INTO Pedido (PedidoID, SolicitanteID, UsuarioID, Descripcion, FechaHora) VALUE (".$this->getID().", ".$this->getSolicitante()->getIdSolicitante().", ".$this->getusuario()->getIDUsuario().", '".$this->getDescripcion()."', '".$this->getFechaHora()."')");
+      $resultid = mysqli_insert_id($conexion);
+      $this->setID($resultid);
+      if($conexion->error){
+         return ("Error: ".$conexion->error);
+          } else {
           return ("Registro insertado correctamente");
-      } else {
-          return ("Error: ".$query->error);
-      }
-   }
-
-   public function delete(){
-      $query = Conexion::conectar()->query("DELETE FROM Pedido WHERE  PedidoID = ".$this->getID());
-      if( $query === TRUE){
-         return ("Registro eliminado correctamente");
-      } else {
-         return ("Error: ".$query->error);
+        }
      }
-   }
-
-   public function update(){
-      $query = Conexion::conectar()->query("UPDATE Pedido SET SolicitanteID = ".$this->getSolicitante()->getID()." usuarioID = ".$this->getusuario()->getID()." Descripcion = ".$this->getDescripcion()."
-      FechaHora = ".$this->getFechaHora()." where IDPedido = ".$this->getID());
-      if( $query === TRUE){
-          return ("Registro actualizado correctamente");
-      } else {
-          return ("Error: ".$query->error);
-      }
-   }
-        
-   static function findByID($id){
-      $response = Conexion::conectar()->query("SELECT * FROM Pedido WHERE IDPedido = ".$_id);
-      if ($response->rowCount() > 0) { 
-         $pedidos = array();
-         while ($row = $response->fetch()) { 
-            array_push($pedidos, new Pedido($row['PedidoID'], Solicitante::findByID($row['SolicitanteID']), Usuario::findByID($row['UsuarioID']), $row['Descripcion'], $row['FechaHora']));
-         }
-      return ($pedidos);
-      }    
-      else { 
-         return ("No existen registros."); 
-      }
-   } 
-
-   static function listarTipoCarga($where){
-      $response = Conexion::conectar()->query("SELECT * FROM Pedido ".$_where);
-      if ($response->rowCount() > 0) { 
-         $pedidos = array();
-         while ($row = $response->fetch()) { 
-            array_push($cargas, new Pedido($row['PedidoID'], Solicitante::findByID($row['SolicitanteID']), Usuario::findByID($row['UsuarioID']), $row['Descripcion'], $row['FechaHora']));
-         }
-      return ($pedidos);
-      }    
-      else { 
-         return ("No existen registros."); 
-      }
-       }
+ 
+     public function delete(){
+      $conexion = Conexion::conectar();
+      $resultado = $conexion->query("DELETE FROM Pedido WHERE PedidoID = ".$this->getID());
+      if($conexion->error){
+         return ("Error: ".$conexion->error);
+          } else {
+          return ("Registro eliminado correctamente");
+        }
      }
-?>
+ 
+     public function update(){
+      $conexion = Conexion::conectar();
+      $resultado = $conexion->query("UPDATE Pedido SETPedidoID = '".$this->getID()."', SolicitanteID = '".$this->getSolicitante()->getIdSolicitante()."', UsuarioID = '".$this->getusuario()->getIDUsuario()."', Descripcion = '".$this->getDescripcion()."', FechaHora ='".$this->getFechaHora()."'");
+         if($conexion->error){
+            return ("Error: ".$conexion->error);
+             } else {
+             return ("Registro actualizado correctamente");
+           }
+     }
+ 
+     public function findAll(){
+      $resultado=Conexion::conectar()->query("SELECT * FROM Pedido");
+         if ($resultado->num_rows > 0) { 
+            $pedido = array();
+            while ($row = $resultado->fetch_assoc()) { 
+               array_push($pedido, new Pedido($row['PedidoID'], Solicitante::findByID($row['SolicitanteID']), Usuario::findByID($row['UsuarioID']), $row['Descripcion'], $row['FechaHora']));
+            }
+         return ($pedido);
+         }    
+         else { 
+            return ("No existen registros."); 
+         }
+     }
+ 
+     public function findById($id){   
+         $resultado=Conexion::conectar()->query("SELECT * FROM Pedido WHERE PedidoID = ".$id);
+         if ($resultado->num_rows > 0) { 
+            $pedido = array();
+            while ($row = $resultado->fetch_assoc()) { 
+               array_push($pedido, new Pedido($row['PedidoID'], Solicitante::findByID($row['SolicitanteID']), Usuario::findByID($row['UsuarioID']), $row['Descripcion'], $row['FechaHora']));
+            }
+         return ($pedido[0]);
+         }
+         else { 
+            return ("No existen registros."); 
+         }     
+     }
+ 
+     public function findAllWhere($where){
+      $resultado=Conexion::conectar()->query("SELECT * FROM Pedido WHERE ".$where);
+      if ($resultado->num_rows > 0) { 
+         $pedido = array();
+         while ($row = $resultado->fetch_assoc()) { 
+            array_push($pedido, new Pedido($row['PedidoID'], Solicitante::findByID($row['SolicitanteID']), Usuario::findByID($row['UsuarioID']), $row['Descripcion'], $row['FechaHora']));
+         }
+      return ($pedido);
+      }    
+      else {
+           return ("No existen registros."); 
+      }     
+  }
+}
+
+  ?>
