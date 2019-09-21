fe6ab21062416a89681e9d38f29709ab0791e814
Nantiare <faguerlsc@gmail.com>
2019-09-20 21:21:36 -0300

    Update TipoCarga.php

--- TrabajoFinal/model/TipoCarga.php
+++ TrabajoFinal/model/TipoCarga.php
@@ -6,11 +6,7 @@ class TipoCarga{
     private $tipoCargaID;
     private $descripcion;
 
-    public function  __construct($descripcion){
-      $this->setDescripcion($descripcion);
-   }
-
-   private function  __construct($id, $descripcion){
+   public function  __construct($id, $descripcion){
       $this->setID($id);
       $this->setDescripcion($descripcion);
    }
@@ -33,7 +29,7 @@ class TipoCarga{
 
      public function insert(){
          $conexion = Conexion::conectar();
-         $resultado = $conexion->query("INSERT INTO TipoCarga(DescripcionCarga) VALUES ('".$this->descripcion."')");
+         $resultado = $conexion->query("INSERT INTO `Tipo Carga` (DescripcionCarga) VALUES ('".$this->descripcion."')");
          $resultid = mysqli_insert_id($conexion);
          $this->setID($resultid);
          if($conexion->error){
@@ -64,7 +60,7 @@ class TipoCarga{
       }
 
       static function findAll(){
-         $resultado=Conexion::conectar()->query("SELECT * FROM TipoCarga");
+         $resultado=Conexion::conectar()->query("SELECT * FROM `Tipo Carga`");
          if ($resultado->num_rows > 0) { 
             $cargas = array();
             while ($row = $resultado->fetch_assoc()) { 
@@ -104,4 +100,31 @@ class TipoCarga{
           }
         }
 }
+
+
+    //ESTO INSERTA UNA CISTERNA 
+    $instaciaPrueba = new TipoCarga(NULL,"Nombre 56");
+   
+    $instaciaPrueba->insert();
+
+    //ESTO BOORA UNA CISTERNAS
+    //$re = Cisterna::findAll()[0]->delete();
+
+    //ESTO TRAE MUCHAS CISTERNAS
+    //$re = Cisterna::findAll();
+
+    //ESTO TRAE UNA CISTERNA POR ID
+    $re = TipoCarga::findAll();
+    var_dump($re);
+    //ESTO TRAE ARRAY CON WHERE STRING
+    //$re = Cisterna::findAllWhere(" NombreCisterna = 'Nombre 1' OR 1 = 1 ");
+/* 
+    $re->setNombreCisterna("Nombre 60");
+
+    $re->update();
+
+    $re2 = Cisterna::findByID(2);
+
+    var_dump($re2); */exit();
 ?>
+
