bd7e14e3cad3c3badfc0dfd582e8bd601c30de4e
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-09-27 18:44:39 -0300

    Update ControllerEmpresa.php

--- TrabajoFinal/controller/ControllerEmpresa.php
+++ TrabajoFinal/controller/ControllerEmpresa.php
@@ -2,34 +2,47 @@
 //--------------------------------------------
 //LLAMO A LA CLASE EMPRESA
 //--------------------------------------------
+require_once ("../view/vistaEmpresa.php");
 require_once ("../model/Empresa.php");
 
-//Comienzo a recopilar los valores del metodo POST.
+//Utilizo el motodo post para obtener datos de formulario. Y luego establecerlos en el servidor.
 switch ($_POST["accion"])
 {
     case ('nuevo'):
-   
-        if (!empty($_POST['razonSocial']) && !empty($_POST['Cuit']) && !empty($_POST['Direccion'])&& !empty($_POST['Telefono']))
-        {
-            if (Empresa::insert($_POST["razonSocial"],$_POST["Cuit"],$_POST['Direccion'],$_POST['Telefono'])
-            {
-            print_r($_POST["razonSocial"],$_POST["Cuit"],$_POST['Direccion'],$_POST['Telefono']);
+        //genero la instancia del objeto Empresa
+        $obj_empresa = new Empresa (null,$_POST['txt_razonSocial'], $_POST['txt_Cuit'], $_POST['txt_Direccion'],$_POST['txt_Telefono']);
+
+        //utilizo el !empty para veificar que una variable no este nula
+        if (!empty($_POST['txt_razonSocial']) && !empty($_POST['txt_Cuit']) && !empty($_POST['txt_Direccion'])&& !empty($_POST['txt_Telefono']))
+        {   
+            
+            //if (Empresa::insert($_POST["txt_razonSocial"],$_POST["txt_Cuit"],$_POST['txt_Direccion'],$_POST['txt_Telefono'])
+            if ($obj_empresa->insert());
+            {   
+                echo "Una Empresa Insertada";
+                var_dump($obj_empresa);
+                print_r($_POST["txt_razonSocial"],$_POST["txt_Cuit"],$_POST['txt_Direccion'],$_POST['txt_Telefono']);
             }
         }
         else
         {
-            echo "es necesario que completes los campos";
+            echo "faltan datos para Guardar en base de datos";
+            var_dump($obj_empresa);
         }
     
     break;
+
     case ('eliminar'):
     
         if (!empty($_POST["IDEmpresa"]))
         {
-            if (Empresa::delete($_POST["IDEmpresa"]))
-        {
+            if (Empresa::delete($_POST["txt_IDEmpresa"]))
+            {   
+            $empre = new Empresa(setIDEmpresa);
+            $empre =setIDEmpresa($_POST["txt_IDEmpresa"]);
             echo 'Empresa eliminada';
-        }
+            var_dump ($_POST);
+            }
         }
         else
         {
@@ -37,7 +50,8 @@ switch ($_POST["accion"])
         }
     
     break;
-    case ('editar'):
+
+    case ('listar'):
     
         if (!empty($_POST["IDEmpresa"]))
         {
@@ -51,5 +65,6 @@ switch ($_POST["accion"])
         }
         }
     break;
+
 }
 ?>
