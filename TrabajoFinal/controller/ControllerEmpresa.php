ad0eda8a8ac817c41068720a6fe0360db25f90bf
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-10-18 21:26:30 -0300

    Update ControllerEmpresa.php

--- TrabajoFinal/controller/ControllerEmpresa.php
+++ TrabajoFinal/controller/ControllerEmpresa.php
@@ -4,33 +4,45 @@
 //--------------------------------------------------------------
 require_once ("../model/Empresa.php");
 
-//-----------------------------------------------
-//IMPORTANTE DEBO SETEAR UN VALOR AL METODO POST DEL ACCION
-//-----------------------------------------------
-//PRUEBAS DEL CONTROLLER EMPRESA
-//----Descomentar la siguiente linea para verificar (insert,delete, lista estan verificados)
-
-//$_POST["accion"]="nuevo";
-//$_POST["accion"]="listar";
-//$_POST["accion"]="eliminar";
-//$_POST["accion"]="update";
-
-//Asigano valores al metodo _POST
-$_POST['txt_razonSocial'] = 'hola' ;
-$_POST['txt_Cuit']='63853';
-$_POST['txt_Direccion']='ruta 22';
-$_POST['txt_Telefono']= '536382';
-
-//Utilizo el motodo post para obtener datos de formulario. Y luego establecerlos en el servidor.
-switch ($_POST["accion"])
+class EmpresaController
 {
+    public function __construct(){
 
-    //-------------------------------------------------------------
-    //$IDEmpresa, $_razonSocial, $_Cuit, $_Direccion, $_Telefono
-    //________________________________________----------------------
+    }
 
+    public function index(){
+        require_once('../model/Empresa.php');
+        $empresa = Empresa::findAll();
+        require_once('../view/viewEmpresaIndex.php');
+    }
+
+    public function nuevo(){
+        echo 'ir a la ruta de creadion de pedido';
+    }
+
+    public function insert ($empresa){
+        require_once('../model/Empresa.php');
+        var_dump("insert:".$empresa->insert()."eir al index de la vista");
+    }
+    public function update($empresa){
+        require_once('../model/Empresa.php');
+        var_dump("update: ".$empresa->update()."e ir al index de la vista");
+    }
+    public function delete($empresa){
+        require_once('../model/Empresa.php');
+        var_dump("delete: ".$empresa->delete(). "e ir al indes de la vista");
+    }
     
+}
 
+//genero el If para setear el formulario
+if(isset($_POST['action'])){
+    $empresaController = new EmpresaController();
+    require_once('../model/Empresa.php');
+    require_once('../model/Solicitante.php');
+    require_once('../model/Usuario.php'); 
+
+    //genero los switch
     case ('nuevo'):
         
         //utilizo el !empty para veificar que una variable no este nula
@@ -51,29 +63,105 @@ switch ($_POST["accion"])
             echo "faltan datos para Guardar en base de datos";
         }
 
-    break;
+        break;
 
-    case ('eliminar'):
-        //seteo la posicion del Objeto
-        $id_obj_eliminar='0';
-        $reso = Empresa::findAll()[$id_obj_eliminar]->delete();
-        var_dump($reso);
-        echo "se elimino una empresa";
-    break;
+        case ('eliminar'):
+            //seteo la posicion del Objeto
+            $id_obj_eliminar='0';
+            $reso = Empresa::findAll()[$id_obj_eliminar]->delete();
+            var_dump($reso);
+            echo "se elimino una empresa";
+        break;
 
-    case ('listar'):
-        $reso = Empresa::findAll();
-        var_dump ($reso);
-        
-    break;
-         case 'update':
-                $id_update ='10';
-                $reso = Empresa::findByID($id_update);
-                //var_dump $reso;
-                $reso->setRazonsocial("ChinoL");
-                $reso->setDireccion("Rivas 986");
-                $reso->update();
-                var_dump($reso); exit;
-         break;
+        case ('listar'):
+            $reso = Empresa::findAll();
+            var_dump ($reso);
+            
+        break;
+            case 'update':
+                    $id_update ='10';
+                    $reso = Empresa::findByID($id_update);
+                    //var_dump $reso;
+                    $reso->setRazonsocial("ChinoL");
+                    $reso->setDireccion("Rivas 986");
+                    $reso->update();
+                    var_dump($reso); exit;
+            break;
 }
+
+
+
+
+//-----------------------------------------------
+//IMPORTANTE DEBO SETEAR UN VALOR AL METODO POST DEL ACCION
+//-----------------------------------------------
+//PRUEBAS DEL CONTROLLER EMPRESA
+//----Descomentar la siguiente linea para verificar (insert,delete, lista estan verificados)
+
+//$_POST["accion"]="nuevo";
+//$_POST["accion"]="listar";
+//$_POST["accion"]="eliminar";
+//$_POST["accion"]="update";
+
+//Asigano valores al metodo _POST
+// $_POST['txt_razonSocial'] = 'hola' ;
+// $_POST['txt_Cuit']='63853';
+// $_POST['txt_Direccion']='ruta 22';
+// $_POST['txt_Telefono']= '536382';
+
+// //Utilizo el motodo post para obtener datos de formulario. Y luego establecerlos en el servidor.
+// switch ($_POST["accion"])
+// {
+
+//     //-------------------------------------------------------------
+//     //$IDEmpresa, $_razonSocial, $_Cuit, $_Direccion, $_Telefono
+//     //________________________________________----------------------
+
+    
+
+//     case ('nuevo'):
+        
+//         //utilizo el !empty para veificar que una variable no este nula
+//         if (!empty($_POST['txt_razonSocial']) && !empty($_POST['txt_Cuit']) && !empty($_POST['txt_Direccion'])&& !empty($_POST['txt_Telefono']))
+//         {
+//             //genero la instancia del objeto Empresa
+//             $obj_empresa = new Empresa ('null',$_POST['txt_razonSocial'], $_POST['txt_Cuit'], $_POST['txt_Direccion'],$_POST['txt_Telefono']);
+
+            
+//             if ($obj_empresa->insert());
+//             {
+//                 var_dump($obj_empresa);
+//                 echo "Se ingreso una empresa";
+//             }
+//         }
+//         else
+//         {
+//             echo "faltan datos para Guardar en base de datos";
+//         }
+
+//     break;
+
+//     case ('eliminar'):
+//         //seteo la posicion del Objeto
+//         $id_obj_eliminar='0';
+//         $reso = Empresa::findAll()[$id_obj_eliminar]->delete();
+//         var_dump($reso);
+//         echo "se elimino una empresa";
+//     break;
+
+//     case ('listar'):
+//         $reso = Empresa::findAll();
+//         var_dump ($reso);
+        
+//     break;
+//          case 'update':
+//                 $id_update ='10';
+//                 $reso = Empresa::findByID($id_update);
+//                 //var_dump $reso;
+//                 $reso->setRazonsocial("ChinoL");
+//                 $reso->setDireccion("Rivas 986");
+//                 $reso->update();
+//                 var_dump($reso); exit;
+//          break;
+// }
 ?>
