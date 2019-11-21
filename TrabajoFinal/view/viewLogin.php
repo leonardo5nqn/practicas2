a60bd38fa3fb27dece8b9768c3759da6c251bdb5
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-11-21 19:48:37 -0300

    Update viewLogin.php

--- TrabajoFinal/view/viewLogin.php
+++ TrabajoFinal/view/viewLogin.php
@@ -5,9 +5,9 @@
 <div align="center">
 <form name="form_login" method="post" action="../controller/controllerLogin.php?action=login">
     <label>Usuario: </label>
-    <input  type="text" name="Usuario">
+    <input  type="text" name="Usuario"><br><br>
     <label>Contrase&ntilde;a</label>
-    <input type="password" name="Contrasena" size="20px" value="" />
+    <input type="password" name="Contrasena" size="20px" value="" /><br><br>
     <input type="submit" name="Iniciar" value="Iniciar Sesi&oacute;n"> 
 </form>
 </div>
