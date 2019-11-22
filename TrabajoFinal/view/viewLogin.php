c9189201efe9d441e8bdae04565d52b0b6a36c3a
diegonqn2019 <50252191+diegonqn2019@users.noreply.github.com>
2019-11-22 11:49:03 -0300

    Update viewLogin.php

--- TrabajoFinal/view/viewLogin.php
+++ TrabajoFinal/view/viewLogin.php
@@ -1,13 +1,22 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+ integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous">
 <br>
 <div align="center">
     <h1>INICIAR SESION</h1>
 </div>
+<br>
 <div align="center">
 <form name="form_login" method="post" action="controllerLogin.php?action=login">
-    <label>Usuario: </label>
-    <input  type="text" name="Usuario"><br><br>
+
+    <label>Usuario</label>
+    <input  type="text" name="Usuario" ><br><br>
     <label>Contrase&ntilde;a</label>
-    <input type="password" name="Contrasena" size="20px" value="" /><br><br>
-    <input type="submit" name="Iniciar" value="Iniciar Sesion"> 
+    <input type="password" name="Contrasena"  value="" /><br><br>
+    <input type="submit" class="btn btn-primary" name="Iniciar" value="Iniciar Sesion"> 
 </form>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
